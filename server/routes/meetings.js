21f8f54d2185c10ba317e4e38b4e1d7492e9ccf5
Fleskara <nikola9100@gmail.com>
2022-07-15 16:48:55 +0200

    Update meetings router module

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -3,6 +3,7 @@ const meetingsController = require('../controllers/meetings');
 
 const router = express.Router();
 
+router.get('/', meetingsController.getAllMeetings);
 router.get('/:id', meetingsController.getMeetingById);
 router.post('/', meetingsController.addNewMeating);
 router.delete('/:id', meetingsController.deleteMeeting);
